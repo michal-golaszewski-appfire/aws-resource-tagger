dc67338505cf1327f11121ae0c12bd39d1274231
michal.golaszewski <michal.golaszewski@appfire.com>
2025-01-22 08:50:36 +0100

    fix: change to full_resource_id in info log

--- taggers/cloudwatch_loggroup_tagger.py
+++ taggers/cloudwatch_loggroup_tagger.py
@@ -4,7 +4,7 @@ from .base_tagger import AwsResourceTagger
 # Concrete class for tagging CloudWatch Log Groups
 class CloudWatchLogGroupTagger(AwsResourceTagger):
     def add_tags(self):
-        print(f"CloudWatch Log Group: {self.resource_id} in region {self.region}")
+        print(f"CloudWatch Log Group: {self.full_resource_id} in region {self.region}")
         logs = boto3.client('logs', region_name=self.region)
         try:
             # Note that the tags must a dict not a list of dicts for the log group
