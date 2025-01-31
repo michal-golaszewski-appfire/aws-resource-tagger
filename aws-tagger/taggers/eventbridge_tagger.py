4822a03ad88089969ae7229f20906fdeaf0794c3
Germán Garcés <german.garces@appfire.com>
2025-01-31 09:50:44 +0100

    feat: add event bridge tagger

---
+++ aws-tagger/taggers/eventbridge_tagger.py
@@ -0,0 +1,18 @@
+import boto3
+from .base import AwsResourceTagger
+from utils.tag_formatter import adapt_tags
+from .registry import TaggerRegistry
+from arn_parser.arn_parser import AWSArnParser
+
+# Concrete class for tagging Event Bridge resources
+@TaggerRegistry.register("events")
+class EventBridgeTagger(AwsResourceTagger):
+    @staticmethod
+    def tag_resource(arn: str, tags: list):
+        print(f"Tagging EventBridge: {arn}")
+        region = AWSArnParser.get_region(arn)
+        function = boto3.client('events', region_name=region)
+        try:
+            function.tag_resource(ResourceARN=arn, Tags=tags)
+        except Exception as e:
+            print(f"Error tagging EventBridge {arn}: {e}")
