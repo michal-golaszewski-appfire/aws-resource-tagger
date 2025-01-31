09fa7e97049cba9677ac6e95ac13a204eb4dc081
Germán Garcés <german.garces@appfire.com>
2025-01-31 11:04:50 +0100

    feat: add dynamodb tagger

---
+++ aws-tagger/taggers/dynamodb_tagger.py
@@ -0,0 +1,18 @@
+import boto3
+from .base import AwsResourceTagger
+from utils.tag_formatter import adapt_ecs_tags
+from .registry import TaggerRegistry
+from arn_parser.arn_parser import AWSArnParser
+
+# Concrete class for tagging ECS Resources
+@TaggerRegistry.register("dynamodb")
+class DynamoDBTagger(AwsResourceTagger):
+    @staticmethod
+    def tag_resource(arn: str, tags: list):
+        print(f"Tagging Dynamo: {arn}")
+        region = AWSArnParser.get_region(arn)
+        dynamo = boto3.client('dynamodb', region_name=region)
+        try:
+            dynamo.tag_resource(ResourceArn=arn, Tags=tags)
+        except Exception as e:
+            print(f"Error tagging Dynamo {arn}: {e}")
